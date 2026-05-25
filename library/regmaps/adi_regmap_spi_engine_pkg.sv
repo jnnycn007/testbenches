ff199d05d8f42e9ce9decfe28ed3f291589f70b5
Laez Barbosa <laez.barbosa@analog.com>
2026-05-25 14:17:49 -0300

    Update SPI Engine version reg (#284)
    * Update SPI Engine version reg
    
    ---------
    
    Signed-off-by: Laez Barbosa <laez.barbosa@analog.com>

--- library/regmaps/adi_regmap_spi_engine_pkg.sv
+++ library/regmaps/adi_regmap_spi_engine_pkg.sv
@@ -1,6 +1,6 @@
 // ***************************************************************************
 // ***************************************************************************
-// Copyright (C) 2014-2024 Analog Devices, Inc. All rights reserved.
+// Copyright 2014 - 2024 (c) Analog Devices, Inc. All rights reserved.
 //
 // In this HDL repository, there are many different and unique modules, consisting
 // of various HDL (Verilog or VHDL) components. The individual modules are
@@ -33,7 +33,7 @@
 // ***************************************************************************
 // ***************************************************************************
 /* Auto generated Register Map */
-/* Wed Jan 29 16:40:28 2025 */
+/* Thu May 21 09:29:28 2026 */
 
 package adi_regmap_spi_engine_pkg;
   import adi_regmap_pkg::*;
@@ -42,9 +42,9 @@ package adi_regmap_spi_engine_pkg;
 /* SPI Engine (axi_spi_engine) */
 
   const reg_t AXI_SPI_ENGINE_VERSION = '{ 'h0000, "VERSION" , '{
-    "VERSION_MAJOR": '{ 31, 16, RO, 'h00000001 },
-    "VERSION_MINOR": '{ 15, 8, RO, 'h00000005 },
-    "VERSION_PATCH": '{ 7, 0, RO, 'h00000003 }}};
+    "VERSION_MAJOR": '{ 31, 16, RO, 'h0001 },
+    "VERSION_MINOR": '{ 15, 8, RO, 'h05 },
+    "VERSION_PATCH": '{ 7, 0, RO, 'h04 }}};
   `define SET_AXI_SPI_ENGINE_VERSION_VERSION_MAJOR(x) SetField(AXI_SPI_ENGINE_VERSION,"VERSION_MAJOR",x)
   `define GET_AXI_SPI_ENGINE_VERSION_VERSION_MAJOR(x) GetField(AXI_SPI_ENGINE_VERSION,"VERSION_MAJOR",x)
   `define DEFAULT_AXI_SPI_ENGINE_VERSION_VERSION_MAJOR GetResetValue(AXI_SPI_ENGINE_VERSION,"VERSION_MAJOR")
